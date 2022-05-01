1e89d5296d635db79bb8064c54a55acacfe285a7
Laura Mazzuca <lauramazzuca@protonmail.com>
2022-05-01 15:42:54 +0200

    completes cosplayer animation

--- Assets/NPCCosplayer.cs
+++ Assets/NPCCosplayer.cs
@@ -6,12 +6,23 @@ public class NPCCosplayer : MonoBehaviour
 {
     [SerializeField]
     private GameObject _cosplayer;
-    //[SerializeField]
-    //private ParticleSystem _changeEffect;
-    // Start is called before the first frame update
+    [SerializeField]
+    private GameObject _charachterMesh;
     void Awake()
     {
-        //_changeEffect.Play();
         _cosplayer.SetActive(false);
+        _charachterMesh.SetActive(false);
+        GetComponent<Animator>().enabled = false;
+
+        StartCoroutine(StartAnimation());
     }
+
+    IEnumerator StartAnimation()
+    {
+        yield return new WaitForSeconds(.5f);
+        GetComponent<Animator>().enabled = true;
+        _charachterMesh.SetActive(true);
+        StopAllCoroutines();
+    }
+
 }
